594df14b7f7b4ecc7f14c8bfddd30c53d1c13ed9
Татьяна Шкуро <shkuro_tanya@mail.ru>
2018-12-23 22:34:38 +0300

    Add files via upload

---
+++ ind_lab_3/ind_lab_3/ind_lab_3.cpp
@@ -0,0 +1,25 @@
+﻿#include<Windows.h>    
+// first include Windows.h header file which is required    
+#include<stdio.h>
+#include "GL/glew.h"
+#include<gl/GL.h>   // GL.h header file    
+#include<gl/GLU.h> // GLU.h header file    
+#include<gl/glut.h>  // glut.h header file from freeglut\include\GL folder    
+#include<conio.h>
+#include<math.h>
+#include<string>
+#include <vector>
+#include <glm/glm.hpp>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <SOIL.h>
+#include <glm/gtc/matrix_transform.hpp>
+
+
+int main()
+{
+    std::cout << "Hello World!\n"; 
+}
